15b3210b6e9875ca6043ea4d55e06927e014fabb
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-03-27 23:23:47 +0300

    a

---
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Feedback extends Component {
+  static defaultProps = {};
+
+  static propTypes = {};
+
+  render() {
+    return <div>Please leave feedback
+        <button ></button>
+        <button ></button>
+        <button ></button>
+    </div>;
+  }
+}
+
+export default Feedback
