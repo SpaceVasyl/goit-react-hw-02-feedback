eb44501e0ecf96ae5347418e433f046f8d12412d
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-03-28 23:12:48 +0300

    1

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
-
+import css from './FeedbackForm.module.css'
 class Feedback extends Component {
-  static defaultProps = {};
-
-  static propTypes = {};
-
+ constructor() {
+  super()
+  this.state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+ }
+  
+goodStat = () => {
+  this.setState({good: +1});
+}
   render() {
-    return <div>Please leave feedback
-        <button ></button>
-        <button ></button>
-        <button ></button>
-    </div>;
+    return (<div>
+      <h2>Please leave feedback</h2>
+        <button type="button" className={css.buttonstat} onClick={this.goodStat}>Good</button>
+        <button type="button" className={css.buttonstat}>Neutral</button>
+        <button type="button" className={css.buttonstat}>Bad</button>
+        <p>{this.state.good}</p>
+    </div>)
   }
 }
 
